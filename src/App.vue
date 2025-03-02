cb8ad903017f57b574d0cf253b43e92483721f0f
Dalton Plitt <daltonp@Daltons-MBP.attlocal.net>
2025-03-02 14:51:49 -0600

    Array change detection

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
 <template>
   <h1>{{ message }}</h1>
   <button @click="sortUsersByAge">Sort users by age</button>
+  <button @click="hideInactiveUsers">Hide inactive users</button>
+  <button @click="showFirstTwoUsers">Show first two users</button>
   <ul>
     <li v-for="(user, index) in users" :key="user.id">
       {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} -
       {{ user.isActive }}
     </li>
   </ul>
-
-  <hr />
-
-  <table>
-    <tr>
-      <th>Index</th>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Operation</th>
-    </tr>
-    <tr v-for="(user, index) in users" :key="user.id">
-      <td>{{ index }}</td>
-      <td>{{ user.id }}</td>
-      <td :class="{ inactive: !user.isActive }">{{ user.name }}</td>
-      <td>{{ user.age }}</td>
-      <td>
-        <button @click="user.isActive = !user.isActive">
-          {{ user.isActive ? 'Deactivate' : 'Restore' }}
-        </button>
-      </td>
-    </tr>
-  </table>
-
-  <hr />
-  <p>Use v-for to iterate through the properties of an object:</p>
-  <div v-for="(value, key, index) in users[0]">
-    {{ index }} - {{ key }}: {{ value }}
-  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello, v-for!')
+let message = ref('Hello, Array Change Detection!')
 
 const users = ref([
   { id: 1001, name: 'John Smith', age: 26, isActive: false },
@@ -51,6 +24,16 @@ const users = ref([
 
 function sortUsersByAge() {
   users.value.sort((a, b) => a.age - b.age)
+}
+
+// filter is a non-mutating method, so we need to replace the old array
+function hideInactiveUsers() {
+  users.value = users.value.filter((user) => user.isActive)
+}
+
+// slice is a non-mutating method, so we need to replace the old array
+function showFirstTwoUsers() {
+  users.value = users.value.slice(0, 2)
 }
 </script>
 
